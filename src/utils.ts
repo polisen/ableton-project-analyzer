8415af50879a98055d61a1860ce848ff18fd61ea
polisen <29376308+polisen@users.noreply.github.com>
2021-08-13 23:36:29 +0200

    fixed logic error in recursiveFormatter

--- src/utils.ts
+++ src/utils.ts
@@ -1,43 +1,38 @@
 const pako = require("pako");
 const txml = require("txml");
 export const bigTask = async (file: any) => {
-  // var zip = new AdmZip("./my_file.zip");
-  // const buffer = await file.arrayBuffer();
-
   if (file) {
     let r = new FileReader();
     r.onload = function (e: any) {
-    //   var contents = e.target.result;
-      console.log(
-        "User layout file:\n" +
-          "name: " +
-          file.name +
-          "\n" +
-          "type: " +
-          file.type +
-          "\n" +
-          "size: " +
-          file.size +
-          " bytes\n"
-      );
-
+      logFileData(file);
       try {
-        if ( file.type == "application/x-gzip" ) {
-            let XMLstring = pako.inflate(new Uint8Array( e.target.result ) , {"to":"string"});
-            console.log(txml.parse(XMLstring))
-            let projectObj = recursiveFormatter('root',txml.parse(XMLstring)) 
-            console.log(projectObj)
+        if (file.type.includes("gzip") || file.name.includes(".als")) {
+          let XMLstring = pako.inflate(new Uint8Array(e.target.result), {
+            to: "string",
+          });
+          const parsedXML = txml.parse(XMLstring);
+          console.log(parsedXML);
+          let projectObj = recursiveFormatter("root", parsedXML);
+          featureExtractor(projectObj);
         } else {
-           // ...
+          // ...
         }
         // ...
-    } catch(e) {
-        console.error(e)
-    }
-    }
-    r.readAsArrayBuffer(file); 
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    r.readAsArrayBuffer(file);
   }
-}
+};
+
+const featureExtractor = (projectObj: any) => {
+  console.log(projectObj);
+  console.log(
+    projectObj.Ableton.LiveSet.Tracks.AudioTrack_15.DeviceChain.MainSequencer
+      .Sample.ArrangerAutomation.Events
+  );
+};
 
 /**
  * Recursively traverses object structure, recursing for each layer, returning a simplified version of the layer.
@@ -45,20 +40,52 @@ export const bigTask = async (file: any) => {
  * @param obj - the processed output of txml.parse()
  */
 
- const recursiveFormatter = function recursiveProjectInfoFormatter(tagName: string, arr: any){
-        if (arr.length === 0) return
-        let obj: any = {}
-        for(let child of arr) {
-            let {children, tagName, attributes} = child;
-            if (!children || !tagName || !attributes) {
-                return child;
-            }
-            if (Object.keys(attributes).length > 0 ) {
-                obj.attributes = attributes
-            }
-            obj = {...obj, [attributes.Id ? `${tagName}_${attributes.Id}`: tagName]: {
-                ...recursiveFormatter(tagName, children)
-            }}
-        }
-        return obj;
-}
+const recursiveFormatter = function recursiveProjectInfoFormatter(
+  tagName: string,
+  arr: any
+) {
+  if (arr instanceof Array !== true) console.log(arr);
+  // console.log(tagName)
+  if (arr.length === 0) return;
+  let obj: any = {};
+  for (let child of arr) {
+    let { children, tagName, attributes } = child;
+    if (!children || !tagName || !attributes) {
+      return child;
+    }
+    if (children.length === 0 && attributes.Value)
+      if (Object.keys(attributes).length > 0) {
+        obj.attributes = attributes;
+      }
+    if (children.length > 0) {
+      obj = {
+        ...obj,
+        [attributes.Id ? `${tagName}_${attributes.Id}` : tagName]: {
+          ...recursiveFormatter(tagName, children),
+        },
+      };
+    } else if (attributes.Value) {
+      obj = { ...obj, [tagName]: attributes.Value };
+    }
+  }
+  return obj;
+};
+
+/**
+ * Logs metadata about file.
+ * @param {file} File - File<Blob>
+ */
+const logFileData = ({ name, type, size }: any) => {
+  console.log(
+    "User layout file:\n" +
+      "name: " +
+      name +
+      "\n" +
+      "type: " +
+      type +
+      "\n" +
+      "size: " +
+      size +
+      " bytes\n"
+  );
+};
